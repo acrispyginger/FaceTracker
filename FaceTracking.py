14f22851fbde5efc762f39cbf08001a5b183d44a
acrispyginger <67551333+acrispyginger@users.noreply.github.com>
2024-02-07 23:33:58 -0700

    Update FaceTracking.py
    Updated 2/7/2024. Added comments. Added Grayscale Frame.

--- FaceTracking.py
+++ FaceTracking.py
@@ -1,27 +1,32 @@
+
+import cv2 as cv
 import cv2
+import time
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+#Getting camera on the first capture port and doing sleep to give camera time to warm up
 cap = cv2.VideoCapture(0)
+time.sleep(5)
 
 while True:
 
+    #Pretty much just a boolean saying that if you dont have the frame then will end the while loop
     ret, frame = cap.read()
-
-    if not ret:
+    if ret == False:
+        print("Failed to grab frame")
         break
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
     
-    cv2.imshow('Face Tracker', frame)
+    #Takes the capture frame and converts it to grayscale. If you want to access the Normal frame display frame if not then do GrayScaleFrame
+    GrayScaleFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    #Shows the frame and the name of the window is in the quote marks
+    cv2.imshow('Big ol Turd', GrayScaleFrame)
+
+    #if q is pressed then end function. 0xFF is an encrypter that will make the ASCII keys represent the same ammount of bits no matter what opperating system. 
+    #ord comes with python and turns the character (for this case 'q' and turns it into a ASCII value to look out for)
+    #if the ASCII value of q (Firgured out by 0xFF) is equal to the ord value then it returns true)
     if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("key pressed")
+        cap.release()
+        cv2.destroyAllWindows()
         break
-
-    cap.release()
-    cv2.destroyAllWindows()
