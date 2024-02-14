dbd8df70bd60da38e4f2e8b2735663923584602b
acrispyginger <67551333+acrispyginger@users.noreply.github.com>
2024-02-14 08:23:59 -0700

    Update FaceTracking.py
    2/14/2024 Model Loaded

--- FaceTracking.py
+++ FaceTracking.py
@@ -8,8 +8,20 @@ import time
 cap = cv2.VideoCapture(0)
 time.sleep(5)
 
-while True:
+#this will change from computer to computer
+CascadePath = 'C:\\Users\\rkuhn\Desktop\\CodingProjects\\ImageShit_OpenVc\\haarcascade_frontalface_default.xml'
 
+#loading the file 
+Cascade = cv2.CascadeClassifier(CascadePath)
+
+#checking if it successfully got the file.
+if Cascade.empty():
+    raise IOError("Unable to load Cascade File")
+else:
+    print("Model Loaded")
+
+while True:
+    
     #Pretty much just a boolean saying that if you dont have the frame then will end the while loop
     ret, frame = cap.read()
     if ret == False:
@@ -18,6 +30,8 @@ while True:
     
     #Takes the capture frame and converts it to grayscale. If you want to access the Normal frame display frame if not then do GrayScaleFrame
     GrayScaleFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face = Cascade.detectMultiScale(GrayScaleFrame, scaleFactor=1.1, minNeighbors=5)
 
     #Shows the frame and the name of the window is in the quote marks
     cv2.imshow('Big ol Turd', GrayScaleFrame)
